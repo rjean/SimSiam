239004c317a1e02b056118d0593cac66ec25bc40
Raphael Jean <raphael.jean@rocketmail.com>
2021-01-04 10:37:28 -0500

    Added FP16 support in evaluation.

--- tools/knn_monitor.py
+++ tools/knn_monitor.py
@@ -1,6 +1,8 @@
 from tqdm import tqdm
 import torch.nn.functional as F 
 import torch
+from torch.cuda.amp import autocast
+
 # code copied from https://colab.research.google.com/github/facebookresearch/moco/blob/colab-notebook/colab/moco_cifar10_demo.ipynb#scrollTo=RI1Y8bSImD7N
 # test using a knn monitor
 def knn_monitor(net, memory_data_loader, test_data_loader, epoch, k=200, t=0.1, hide_progress=False):
@@ -11,7 +13,8 @@ def knn_monitor(net, memory_data_loader, test_data_loader, epoch, k=200, t=0.1, 
     with torch.no_grad():
         # generate feature bank
         for data, target in tqdm(memory_data_loader, desc='Feature extracting', leave=False, disable=hide_progress):
-            feature = net(data.cuda(non_blocking=True))
+            with autocast():
+                feature = net(data.cuda(non_blocking=True))
             feature = F.normalize(feature, dim=1)
             feature_bank.append(feature)
             target_bank.append(target)
@@ -23,9 +26,10 @@ def knn_monitor(net, memory_data_loader, test_data_loader, epoch, k=200, t=0.1, 
         # loop test data to predict the label by weighted knn search
         test_bar = tqdm(test_data_loader, desc='kNN', disable=hide_progress)
         for data, target in test_bar:
-            data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
-            feature = net(data)
-            feature = F.normalize(feature, dim=1)
+            with autocast():
+                data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
+                feature = net(data)
+                feature = F.normalize(feature, dim=1)
             
             pred_labels = knn_predict(feature, feature_bank, feature_labels, classes, k, t)
 
